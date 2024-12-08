fa28f38e2f9b10e908fe7c927853fb9163deabc8
ohrise <lydia032789@naver.com>
2024-12-09 00:55:29 +0900

    마지막

--- server/repositories/FireinformationRepositories.js
+++ server/repositories/FireinformationRepositories.js
@@ -3,7 +3,7 @@ const xlsx = require('xlsx');
 const pool = require('../pgConnect.js'); // PostgreSQL 연결
 
 // 엑셀 파일 경로
-const excelFilePath = './지역정보_최종.xlsx';
+const excelFilePath = './location.xlsx';
 
 const workbook = xlsx.readFile(excelFilePath);
 const sheet = workbook.Sheets[workbook.SheetNames[0]];
